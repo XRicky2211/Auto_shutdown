af6a7c52bc15059faa0a6ca752986a959855007b
Ricky <2019965001@qq.com>
2026-06-19 21:42:59 +0800

    更新 2026-06-19 21:42:59

--- Git管理工具.bat
+++ Git管理工具.bat
@@ -1,4 +1,4 @@
 @echo off
 title Auto Shutdown Git 管理工具 - 推送/拉取/分支/回退
-pwsh -ExecutionPolicy Bypass -NoLogo -File "%~dp0git-manager.ps1"
+"%ProgramFiles%\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -NoLogo -File "%~dp0git-manager.ps1"
 pause
